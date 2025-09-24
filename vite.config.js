121d4549be1050f2be76515e4a3756cabb068097
sherahchen123 <sherahchen@outlook.com>
2025-09-24 21:31:20 +0800

    changetodeploy

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,47 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
-  base: '/personal-website-c/',
+  base: "/personal-website-c/",
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
+        format: "es", // 确保输出 ES 模块格式
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].[hash].js",
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
           }
-        }
-      }
-    }
+        },
+      },
+    },
   },
   server: {
     port: 3000,
     open: true,
     proxy: {
-      '/api': {
-        target: 'https://api.example.com',
+      "/api": {
+        target: "https://api.example.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   optimizeDeps: {
-    include: ['gsap', 'axios', 'aos'],
+    include: ["gsap", "axios", "aos"],
   },
 });
